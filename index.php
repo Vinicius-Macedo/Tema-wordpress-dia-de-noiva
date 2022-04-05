140e501312a0dc4fcc5657c471975ececedb0a4e
viniciusmweb <vinicius.silva1076@etec.sp.gov.br>
2022-04-05 11:56:51 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Main template file.
+ * 
+ * @package Dia de noiva
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dia de noiva</title>
+</head>
+<body>
+  
+</body>
+</html>
